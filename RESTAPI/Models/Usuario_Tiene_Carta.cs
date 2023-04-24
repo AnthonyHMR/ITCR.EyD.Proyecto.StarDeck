ab1b0ffc2a415e932192ff781cda83d032149deb
Anthony <anthonymoro08@gmail.com>
2023-04-24 07:28:58 +0000

    [ADD] #1 Creación de la clase Usuario_Tiene_Carta
    Se crea el modelo y el controlador para la conexión entre las tablas Carta y Usuario

---
+++ RESTAPI/Models/Usuario_Tiene_Carta.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RESTAPI.Models
+{
+    public class Usuario_Tiene_Carta
+    {
+        [Key, Column(Order = 1)]
+        public string id_usuario { get; set; }
+        [Key, Column(Order = 2)]
+        public string id_carta { get; set; }
+    }
+}
