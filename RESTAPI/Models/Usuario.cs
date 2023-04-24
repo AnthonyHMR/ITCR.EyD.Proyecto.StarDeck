a9da59131fe3e571704d7920980be7fe62d92c2e
Anthony <anthonymoro08@gmail.com>
2023-04-24 10:09:04 +0000

    [UPDATE] #1 Actualización de clases
    Se actualizan ciertas variables en las clases y se comienza con las pruebas para la conexión a la base de datos

--- RESTAPI/Models/Usuario.cs
+++ RESTAPI/Models/Usuario.cs
@@ -10,7 +10,10 @@ namespace RESTAPI.Models
         public string nombre_usuario { get; set; }
         public string nombre_completo { get; set; }
         public string nacionalidad { get; set; }
-        public int contrasena { get; set; }
-        public string? Imagen { get; set; }
+        public string contrasena { get; set; }
+        public string? imagen { get; set; }
+        public string estado { get; set; }
+        public int ranking { get; set; }
+        public int monedas { get; set; }
     }
 }
