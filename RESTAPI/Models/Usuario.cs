bf3bf9dc734fcccd5a61cdffdd8b3579d41f7b81
Anthony <anthonymoro08@gmail.com>
2023-04-24 07:27:32 +0000

    [ADD] #1 Creación de la clase Usuario
    Se crea el modelo y el controlador para la conexión de la tabla Usuario

---
+++ RESTAPI/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml;
+
+namespace RESTAPI.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public string id_usuario { get; set; }
+        public string nombre_usuario { get; set; }
+        public string nombre_completo { get; set; }
+        public string nacionalidad { get; set; }
+        public int contrasena { get; set; }
+        public string? Imagen { get; set; }
+    }
+}
