0646d8e0ccb7d7aa25ab98022597ddc53661c4c9
Victor Castrillo <victor.castrillo.99@gmail.com>
2023-04-25 22:34:47 -0600

    [UPDATE] GUI

--- StarDeck/Assets/Code/GUIManager.cs
+++ StarDeck/Assets/Code/GUIManager.cs
@@ -3,33 +3,7 @@ using TMPro;
 
 public class GUIManager : MonoBehaviour
 {
-    [SerializeField] public GameObject _register;
-    [SerializeField] public GameObject _createCard;
-    [SerializeField] public TMP_InputField username;
-    [SerializeField] public TMP_InputField name;
+    [SerializeField] public GameObject[] _windows;
 
-    public void SwitchScreen ()
-    {
-        if (_register.activeSelf)
-        {
-            _register.SetActive(false);
-            _createCard.SetActive(true);
-        }
-        else 
-        {
-            _createCard.SetActive(false);
-            _register.SetActive(true);
-        }
-    }
-
-    public void PrintUser ()
-    {
-        Debug.Log(GetInfo());
-    }
-
-    public string GetInfo () 
-    {
-        string output = "{user: "+ username.text + ", name: " + name.text + "}";
-        return output;
-    }
+    
 }
